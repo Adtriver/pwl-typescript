2b3c9e2f3862fccb5267a65ac8cc2b4191e9c90e
Iqlima Nur'ain <h1101241007@student.untan.ac.id>
2026-02-25 04:43:36 +0000

    index

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return 'Welcome ${nama} di PWL';
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function hello(nama: string): string {
+    return `Welcome ${nama} di PWL`;
+}
+const mahasiswa: string = "Hannibal";
+const umur: number = 20;
+
+console.log(hello(mahasiswa));
+console.log(`Nama: ${mahasiswa}, Umur: ${umur} tahun`);
